d408396d717f2424c4722b1f302fd3f159996970
Artur Furmańczyk <furmanczyk.artur@gmail.com>
2018-12-18 20:47:38 +0100

    Add day names, etc

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Weather, Item } from '../shared/weather';
+import { Weather, Item, Condition } from '../shared/weather';
 import { WeatherCodeParserService } from '../shared/weather-code-parser.service';
 
 @Component({
@@ -13,6 +13,9 @@ export class WeatherCardComponent implements OnInit {
   @Input() weather: Weather;
 
   lastUpdated: string;
+  current: Condition;
+  currentTemp: number;
+  windSpeed: number;
   daysOfWeek: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   constructor(private codeParser: WeatherCodeParserService) { }
@@ -20,9 +23,28 @@ export class WeatherCardComponent implements OnInit {
   ngOnInit() {
     // TODO: Parse weather codes to icons
     this.lastUpdated = new Date(this.weather.created).toLocaleString();
+    this.current = this.weather.channel.item.condition;
+    this.currentTemp = this.getCelsiusTemp(this.current.temp);
+    this.windSpeed = this.getWindSpeedInKmph(this.weather.channel.wind.speed);
   }
 
   getWeatherClass(code: number): string {
-    return `icon ${this.codeParser.parseCode(code)}`;
+    return `icon ${this.codeParser.parseCode(code.toString())}`;
+  }
+
+  getCelsiusTemp(fTemp: number): number {
+    return Math.round((Math.round(fTemp) - 32) / 1.8);
+  }
+
+  getWindSpeedInKmph(mphSpeed: number): number {
+    return Math.round(mphSpeed * 1.609344);
+  }
+
+  getDayOfWeek(index: number) {
+    if (this.weather.channel.item.forecast[index]) {
+      const dayNumber = new Date(this.current.date).getDay();
+      console.log(dayNumber);
+      return this.daysOfWeek[(index + dayNumber) % 7];
+    }
   }
 }
